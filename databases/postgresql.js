146e8d3cccff38aba9467a9da6070b9cb6ee82bd
Ever Vega <evervegasuazo@gmail.com>
2023-07-17 23:31:31 -0600

    Table transactions created

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -2,9 +2,40 @@ const { newDb } = require('pg-mem');
 
 const postgresql = newDb();
 
-// create mock data
-postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
-                insert into users values (1, 'Juan', true);`);
+// Create mock data
+
+// Create users table
+postgresql.public.none(`
+    create table users(
+        pk_user integer,
+        name text, 
+        status boolean
+    );
+
+    insert into users values (
+        1, 
+        'Juan',
+        true
+    );
+`);
+
+//Create transaction table
+postgresql.public.none(`
+    create table transactions(
+        pk_transaction integer,
+        fk_user integer,
+        description text,
+        amount float
+    );
+
+    insert into transactions values (
+        1, 
+        1,
+        'transaction test',
+        1234.56
+    );
+`);
+
 
 
 module.exports = {
